a4f5c6ee7ec879d94b4fecd37321df25bbaca486
s1049235@haileybury.vic.edu.au <s1049235@haileybury.vic.edu.au>
2024-06-11 01:35:36 +0000

    Created formMainApp.formNewItem; Edited settings and formMainApp

--- client_code/formMainApp/__init__.py
+++ client_code/formMainApp/__init__.py
@@ -1,5 +1,6 @@
 from ._anvil_designer import formMainAppTemplate
 from anvil import *
+from .formNewItem import formNewItem
 
 
 class formMainApp(formMainAppTemplate):
@@ -8,3 +9,8 @@ class formMainApp(formMainAppTemplate):
     self.init_components(**properties)
 
     # Any code you write here will run before the form opens.
+
+  def btnNewItem_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    alert(content=formNewItem(),
+    title="New Item")
